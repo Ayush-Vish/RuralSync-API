603ef8543f3231f9f1b1ba88d7cb2e17f97a3192
Eyeaadil <madil9227583@gmail.com>
2024-10-12 11:12:10 +0530

    agent-service define

--- db/src/lib/models/agent.model.ts
+++ db/src/lib/models/agent.model.ts
@@ -2,98 +2,6 @@ import { hash } from "bcrypt";
 import { sign } from "jsonwebtoken";
 import mongoose from "mongoose";
 
-<<<<<<< HEAD
-// Define the schema for the Agent
-const agentSchema = new mongoose.Schema({
-  agentId: {
-    type: String,
-    // required: true,
-    unique: true
-  },
-  name: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: { type: String, required: true },
-  phoneNumber: {
-    type: String,
-    required: true
-  },
-  address: {
-    type: String,
-    required: true
-  },
-  location: {
-    latitude: {
-      type: Number,
-      required: true
-    },
-    longitude: {
-      type: Number,
-      required: true
-    }
-  },
-  services: {
-    type: [String], // Array of services like coolerRepair, washingMachineRepair
-    required: true
-  },
-  serviceArea: {
-    type: String,
-    required: true
-  },
-  availability: {
-    type: String,
-    required: true
-  },
-  serviceProviderId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'ServiceProvider',
-    required: true
-  },
-  customerId: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Customer'
-  }],
-  rating: {
-    type: Number,
-    default: 0
-  },
-  feedback: [{
-    type: String
-  }],
-  currentBookings: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Booking'
-  }],
-  completedBookings: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Booking'
-  }],
-  createdAt: {
-    type: Date,
-    default: Date.now
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now
-  }
-});
-
-// Create a pre-save middleware to update the updatedAt field
-agentSchema.pre('save', function(next) {
-    this.updatedAt = new Date();
-    next();
-  });
-  
-
-// Create the Agent model using the schema
- export const Agent = mongoose.model('Agent', agentSchema);
-=======
 const { Schema } = mongoose;
 
 const agentSchema = new Schema({
@@ -130,6 +38,5 @@ agentSchema.pre('save', async function (next) {
   });
 
 export const Agent = mongoose.model('Agent', agentSchema);
->>>>>>> 3845ca4fad34cbe79340011b3c8726bbbaf5a078
 
 
