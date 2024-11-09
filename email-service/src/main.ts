207481b93659d7a061bddb50f4465824154bbe00
eyeaadil <madil9227583@gmail.com>
2024-11-09 17:59:01 +0530

    fix

--- email-service/src/main.ts
+++ email-service/src/main.ts
@@ -3,12 +3,13 @@
 import express from 'express';
 import * as path from 'path';
 import emailRoutes from './routes/routes';
+import { connectToDb } from '@org/db';
 const app = express();
 import dotenv from 'dotenv';
 import bodyparser from 'body-parser';
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
-
+connectToDb()
 dotenv.config();
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
